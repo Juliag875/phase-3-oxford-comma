9d39a6b75685619595bc4a763367f628c7163fca
Iuliia El Sammak <juliag875@gmail.com>
2022-02-22 22:27:49 -0500

    commit tree a52dab0fd288b400b37267d99b5f47f81ab98725

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
-def oxford_comma(array)
-
+def oxford_comma(list)
+  return list.join(" and ") if list.length < 3
+  list[-1] = "and #{list[-1]}"
+  list.join(", ")
 end
+
+puts oxford_comma(["fiddleheads", "okra", "kohlrabi"])
+
